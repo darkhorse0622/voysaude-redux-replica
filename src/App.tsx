228ef8f6fa846932ce6f759c3ddefab803843158
darkhorse0622 <darkhorse0622@gmail.com>
2025-06-22 02:45:33 +0900

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 import Index from "./pages/Index";
 import Treatment from "./pages/Treatment";
 import NotFound from "./pages/NotFound";
@@ -11,18 +13,20 @@ import NotFound from "./pages/NotFound";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/treatments" element={<Treatment />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+  <QueryClientProvider client={ queryClient }>
+    <Provider store={store}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/treatments" element={<Treatment />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+        </TooltipProvider>
+      </Provider>
   </QueryClientProvider>
 );
 
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,8 @@
-
-import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from '@/store/store';
 import Dashboard from './Dashboard';
 
 const Index = () => {
   return (
-    <Provider store={store}>
       <Dashboard />
-    </Provider>
   );
 };
 
